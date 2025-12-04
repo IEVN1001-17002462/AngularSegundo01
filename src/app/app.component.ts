c34222daf50525f8c1b42cd103fd8a0a3f4f83c3
Rafael Gonzalez <rafaeelhdez@gmail.com>
2025-12-03 23:51:25 -0600

    AngularSegundoTerminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,12 @@ import { AporbComponent } from './formularios/aporb/aporb.component';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ZodiacoComponent } from './formularios/zodiaco/zodiaco.component';
+import { TemhComponent } from "./tem/temh/temh.component";
+import { TempComponent } from "./tem/temp/temp.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, DistanciaComponent, ZodiacoComponent],
+  imports: [RouterOutlet, NavbarComponent, TemhComponent, TempComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,53 +1,20 @@
 import { Routes } from '@angular/router';
-
+ 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        children: [
-            {
-                path: 'sign-in',
-                loadComponent:() => import('./auth/features/sign-in/sign-in.component').then((c) => c.SignInComponent),
-            },
-            {
-                path: 'sign-up',
-                loadComponent: () => import('./auth/features/sign-up/sign-up.component').then( (c) =>c.SignUpComponent),
-            }
-        ]
-    },
-    {
-        path:'utl',
-        children:[
-            {
-                path: 'listaalumnos', loadComponent: () => import('./utl/alumnos/alumnos.component').then((c) => c.AlumnosComponent),
-            },
-            {
-                path: 'agregar', loadComponent: () => import('./utl/agregar/agregar.component').then((c) => c.AgregarComponent),
-            },
-            {
-                path: 'eliminar/matricula', loadComponent: () => import('./utl/eliminar/eliminar.component').then((c) => c.EliminarComponent),
-            },
-            {
-                path: 'editar/matricula', loadComponent: () => import('./utl/editar/editar.component').then((c) => c.EditarComponent),
-            },
-        ]
-    },
-    {
-        path: 'formularios',
-        children:[
-            {
-                path: 'aporb', loadComponent: () => import('./formularios/aporb/aporb.component').then((c) => c.AporbComponent)
-            },
-            {
-                path: 'distancia', loadComponent: () => import('./formularios/distancia/distancia.component').then((c) => c.DistanciaComponent)
-            },
-            {
-                path: 'zodiaco', loadComponent: () => import('./formularios/zodiaco/zodiaco.component').then((c) => c.ZodiacoComponent)
-            },
-            {
-                path: 'multiplicacion', loadComponent: () => import('./formularios/multiplicacion/multiplicacion.component').then((c) => c.MultiplicacionComponent)
-            }
-        ]
-    },
-    { path: '', redirectTo: 'admin', pathMatch: 'full' },
-    { path: '**', redirectTo: 'admin' },
+ /*  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/features/auth.routes').then((m) => m.default),
+  }, */
+  {
+    path: 'utl',
+    loadChildren: () =>
+      import('./utl/utl.routes').then((m) => m.default),
+  },
+ /*  {
+    path: 'formularios',
+    loadChildren: () =>
+      import('./formularios/formularios.routes').then((m) => m.default),
+  }, */
 ];
+ 
